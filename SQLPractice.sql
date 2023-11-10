738e616131b9231d313a179a5cf801c4a6f46aca
winnie cheng <winniecheng@winnietekimbp.lan>
2023-11-09 16:48:23 -0800

    <update SQL practice file >

--- SQLPractice.sql
+++ SQLPractice.sql
@@ -1,11 +1,15 @@
-SELECT s.name, COUNT(*), SUM(o.total_amt_usd) total_spent, 
-        CASE WHEN COUNT(*) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'top'
-        WHEN COUNT(*) > 150 OR SUM(o.total_amt_usd) > 500000 THEN 'middle'
-        ELSE 'low' END AS sales_rep_level
-FROM orders o
-JOIN accounts a
-ON o.account_id = a.id 
-JOIN sales_reps s
-ON s.id = a.sales_rep_id
-GROUP BY s.name
-ORDER BY 3 DESC;
+/*Quizzes CONCAT
+Question 3. We would also like to create an initial password, which they will change after their first log in. 
+The first password will be the first letter of the primary_poc's first name (lowercase), then the last letter of their first name (lowercase), 
+the first letter of their last name (lowercase), 
+the last letter of their last name (lowercase), 
+the number of letters in their first name, the number of letters in their last name, 
+and then the name of the company they are working with, all capitalized with no spaces.*/
+
+WITH t1 AS (
+	        SELECT primary_poc,LEFT(primary_poc, POSITION(' ' IN primary_poc)-1)first_name, 
+	        RIGHT(primary_poc, LENGTH(primary_poc) - POSITION(' 'IN primary_poc))last_name,name
+            FROM accounts)
+SELECT first_name, last_name, LEFT(LOWER(first_name),1)|| RIGHT(LOWER(first_name),1)|| LEFT(LOWER(last_name),1)||
+									  RIGHT(LOWER(last_name),1)|| LENGTH(first_name)|| LENGTH(last_name)|| REPLACE(UPPER(name), ' ','')
+FROM t1;
